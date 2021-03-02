2c12abac81ee1396033e0519664db49fa48533af
demo <fritsler.dmitri@yandex.ru>
2021-03-02 18:46:02 +0500

    reboot_homework_4

--- homework/homework_4.cpp
+++ homework/homework_4.cpp
@@ -1,5 +1,5 @@
 //
-// Created by dmitri on 25.02.2021.
+// Created by dmitri on 02.03.2021.
 //
 #include <iostream>
 #include<cmath>
@@ -11,32 +11,49 @@ int main () {
 	std::cout << "Enter the number b: ";
 	std::cin  >> b;
 	sum = a + b;
-	std::cout << ((sum >= 10 && sum <= 20) ? "true" : "false") << std::endl;
+	if (sum >= 10 && sum <= 20) {
+		std::cout << "true" << std::endl;
+	} else {
+		std::cout << "false" << std::endl;
+	}
 
-    int num, i;
-    bool isPrime = 1;
-    std::cout<<"Enter a number: ";
-    std::cin>>num;
-    for (i=2; i<=(sqrt(abs(num))); i++){
-        if (num%i==0){
-        isPrime = 0;
-        break;
+ 	int num, i;
+	bool isPrime = 1;
+	std::cout<<"Enter a number: ";
+	std::cin>>num;
+	for (i=2; i<=(sqrt(abs(num))); i++){
+		if (num%i==0){
+			isPrime = 0;
+		break;
         }
     }
-        std::cout << ((isPrime) ? "This is a prime number" : "This is not a prime number") << std::endl;
+	if (isPrime) {
+		std::cout << "This is a prime number" << std::endl;
+	} else {
+		std::cout << "This is not a prime number" << std::endl;
+	}
+
         
         int x, y;
         std::cout << "Enter the number x: ";
         std::cin >> x;
         std::cout << "Enter the number y: ";
         std::cin >> y;
-        std::cout << ((x == 10 || y == 10 || (x + y) == 10) ? "true" : "false") << std::endl;
+        if (x == 10 || y == 10 || (x + y) == 10) {
+        	std::cout << "true" << std::endl;
+        } else {
+        	std::cout << "false" << std::endl;
+        }
         
         int year;
         std::cout << "Enter the year: ";
         std::cin >> year;
-        std::cout << ((year % 4 == 0) ? "this year is a leap year according to the old calendar" : "this year is not a leap year according to the old calendar") << std::endl;
-        std::cout << ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0) ? "this year is a leap year according to the new calendar" : "this year is not a leap year according to the new calendar") << std::endl;
+        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+        	std::cout << "this year is a leap year" << std::endl;
+        } else {
+        	std::cout << "this year is not a leap year" << std::endl;
+        }
+
     return 0;
 }
 
